5ea898bd547ca16101d9956b3d204ec089ee1c86
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2016-11-30 16:32:46 +0300

    plugin for neovim: update mode

--- vector_nvim.py
+++ vector_nvim.py
@@ -2,6 +2,7 @@ import neovim
 
 import re
 import socket
+import os
 
 @neovim.plugin
 class Vector:
@@ -10,15 +11,27 @@ class Vector:
 		self.vim = vim
 		self.ip = "192.168.3.116"
 		self.port = "20000"
+		self.os = "windows"
 
 	@neovim.function("VectorSetVar", sync=True)
 	def setvar(self, args):
 		if args[0] == "windows":
+			self.os = "windows"
 			self.ip = "192.168.3.116"
 			self.port = "20000"
 		else:
+			self.os = "linux"
 			self.ip = "192.168.3.115"
 			self.port = "20000"
+	
+	def get_path(self,path=None):
+		if path == None:
+			path = os.getcwd()
+
+		if self.os == "windows":
+			return path.replace("/home/nozdrin","e:")
+		else:
+			return path
 
 	def send(self, message):
 		sock = socket.socket()
@@ -27,6 +40,24 @@ class Vector:
 		sock.settimeout(None)
 		sock.send(message.encode("utf-8") + b'\n# TCP-END\n')
 		sock.close
+
+	@neovim.function("VectorUpdate", sync=True)
+	def vectorupdate(self, args):
+		if args[0] == "all":
+			text = "Git.export '" + self.get_path() + "'"
+		else:
+			#buffer = self.vim.current.buffer.name
+			buffer = self.vim.current.buffer
+			cpath = os.getcwd() # current path
+			text = 'Git.export %q{' + self.get_path() + '},%q{'
+			#text += buffer.replace(cpath, '') + '}'
+			#text  += buffer[0]
+			#i = 0
+			ch = re.match(r'^(\w*)\.\w*\s+[":](\w*)"?\s+do\s*$',buffer[0])
+			text += ch.group(1) + '/' + ch.group(2) + '}'
+
+		self.vim.command("echom '" + text + "'")
+		self.send(text)
 
 	@neovim.function("Vector", sync=True)
 	def vector(self, args):
@@ -96,5 +127,5 @@ class Vector:
 
 		result += '\n'.join(buffer[sline+1:eline]) + '\n'
 
-		result +=  buffer[eline].replace(',','',1)
+		result +=  buffer[eline].replace(',','',1) + '\nend'
 		return result
